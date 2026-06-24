741353c916d55d6edb9de972b64644c491e04b47
Stepan Ananin <xrf848@gmail.com>
2026-06-25 00:01:51 +0300

    [firewall] add ETW DNS monitor for DoH support: subscribe to Microsoft-Windows-DNS-Client events to capture all DNS resolutions regardless of protocol

--- daemon/firewall/src/firewall.c
+++ daemon/firewall/src/firewall.c
@@ -20,6 +20,7 @@
 #include "../include/common.h"
 #include "../include/allowlist.h"
 #include "../include/dns.h"
+#include "../include/dns_etw.h"
 #include "../include/ipc.h"
 #include "../../common/log/log.h"
 
@@ -630,6 +631,9 @@ void WINAPI ServiceMain(DWORD argc, LPTSTR *argv) {
 
     OutputDebugString("[ServiceMain] Starting IPC");
     IpcStart(g_ServiceStopEvent);
+
+    OutputDebugString("[ServiceMain] Starting ETW DNS monitor");
+    DnsEtwStart(g_ServiceStopEvent);
 
     g_ClientStopEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
 
