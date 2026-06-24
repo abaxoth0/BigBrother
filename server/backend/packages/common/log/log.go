677a8278ad33e055c8ea3324e1bdf612eb59f84c
Stepan Ananin <xrf848@gmail.com>
2026-06-24 23:11:40 +0300

    [server/fix] use filepath.ToSlash for Windows exe path; add logs\server dir to NSIS installer

--- server/backend/packages/common/log/log.go
+++ server/backend/packages/common/log/log.go
@@ -3,7 +3,7 @@ package log
 import (
 	"fmt"
 	"os"
-	"path"
+	"path/filepath"
 
 	"github.com/abaxoth0/Ain/logger"
 )
@@ -17,9 +17,9 @@ var DefaultLoggerConfig = &logger.FileLoggerConfig{
 	Path: func() string {
 		exe, err := os.Executable()
 		if err == nil {
-			return path.Join(path.Dir(exe), "logs", "server")
+			return filepath.ToSlash(filepath.Join(filepath.Dir(exe), "logs", "server"))
 		}
-		return path.Join(".", "logs", "server")
+		return filepath.ToSlash(filepath.Join(".", "logs", "server"))
 	}(),
 	FilePerm: 0600, // Group and Other bits are ignored on windows
 	LoggerConfig: &logger.LoggerConfig{
